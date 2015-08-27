e0f75921f23ea6ba6124fbf635e447ca8cd16407
Ivan Franjić <ivan.franjic@it-engineer.org>
2015-08-27 09:48:38 +0200

    Modify InstallXEwsModule.ps1

--- InstallXEwsModule.ps1
+++ InstallXEwsModule.ps1
@@ -1,50 +1,44 @@
-﻿function Install-XEwsModule
+﻿[CmdletBinding()]
+param
+(
+
+)
+
+$moduleFiles = @("Microsoft.Exchange.WebServices.dll", "Microsoft.Exchange.WebServices.xml", "XEws.dll", "XEws.format.ps1xml", "XEws.psd1", "en-US/XEws.dll-Help.xml");
+$githubEwsEndpoint = "https://github.com/IvanFranjic/XEws/tree/master/bin/Debug";
+$moduleHomeFolder = [String]::Format("{0}\XEws", $env:PSModulePath.Split(";") -match $env:USERNAME);
+
+Write-Verbose -Message "Checking if module home folder exist...";
+
+if (!(Test-Path $moduleHomeFolder))
 {
-
-    [CmdletBinding()]
-    param
-    (
-
-    )
-
-    $moduleFiles = @("Microsoft.Exchange.WebServices.dll", "Microsoft.Exchange.WebServices.xml", "XEws.dll", "XEws.format.ps1xml", "XEws.psd1", "en-US/XEws.dll-Help.xml");
-    $githubEwsEndpoint = "https://github.com/IvanFranjic/XEws/tree/master/bin/Debug";
-    $moduleHomeFolder = [String]::Format("{0}\XEws", $env:PSModulePath.Split(";") -match $env:USERNAME);
-
-    Write-Verbose -Message "Checking if module home folder exist...";
-
-    if (!(Test-Path $moduleHomeFolder))
+    try
     {
-        try
-        {
-            $null = New-Item -ItemType "Directory" -Path ($env:PSModulePath.Split(";")[0]) -Name "XEws" -Force -ErrorAction Stop;
-            $null = New-Item -ItemType "Directory" -Path $moduleHomeFolder -Name "en-US" -Force;
-        }
-        catch
-        {
-            throw "Error encountered: $($_.Exception.Message)";
-        }
+        $null = New-Item -ItemType "Directory" -Path ($env:PSModulePath.Split(";")[0]) -Name "XEws" -Force -ErrorAction Stop;
+        $null = New-Item -ItemType "Directory" -Path $moduleHomeFolder -Name "en-US" -Force;
     }
-
-    Write-Verbose -Message "Initializing web client...";
-
-    $webClient = New-Object System.Net.WebClient;
-
-    foreach ($item in $moduleFiles)
+    catch
     {
-        $moduleFileLocation = [String]::Format("{0}/{1}", $githubEwsEndpoint, $item);
-        
-        if ($item.Contains("/"))
-        {
-            $item = $item.Replace("/", "\");
-        }
-
-        Write-Verbose -Message "Downloading file $item";
-
-        $fileLocation = [String]::Format("{0}\{1}", $moduleHomeFolder, $item);
-        $fileData = $webClient.DownloadData($moduleFileLocation);       
-        Set-Content -Value $fileData -Path $fileLocation -Encoding Byte -Force;
+        throw "Error encountered: $($_.Exception.Message)";
     }
 }
 
-Install-XEwsModule -Verbose
+Write-Verbose -Message "Initializing web client...";
+
+$webClient = New-Object System.Net.WebClient;
+
+foreach ($item in $moduleFiles)
+{
+    $moduleFileLocation = [String]::Format("{0}/{1}", $githubEwsEndpoint, $item);
+        
+    if ($item.Contains("/"))
+    {
+        $item = $item.Replace("/", "\");
+    }
+
+    Write-Verbose -Message "Downloading file $item";
+
+    $fileLocation = [String]::Format("{0}\{1}", $moduleHomeFolder, $item);
+    $fileData = $webClient.DownloadData($moduleFileLocation);       
+    Set-Content -Value $fileData -Path $fileLocation -Encoding Byte -Force;
+}
