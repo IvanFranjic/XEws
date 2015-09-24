eb4a2ad4628c53cac06899e81889a66857ad5ef2
IvanF <ivan.franjic@it-engineer.org>
2015-09-24 06:36:54 +0200

    minor fixes, addes remove-xewssession cmdled

---
+++ Cmdlet/RemoveXEwsSession.cs
@@ -0,0 +1,30 @@
+﻿namespace XEws.Cmdlet
+{
+    using System.Management.Automation;
+
+    [Cmdlet( VerbsCommon.Remove , "XEwsSession" )]
+    public sealed class RemoveXEwsSession : PSCmdlet
+    {
+        protected override void ProcessRecord ( )
+        {
+            if (this.SessionVariableExist())
+            {
+                WriteWarning( string.Format( "Removing session variable." ) );
+                this.RemoveSessionVariable( );
+            }
+        }
+
+        internal bool SessionVariableExist()
+        {
+            if ( this.SessionState.PSVariable.Get( "EwsSession" ) != null )
+                return true;
+
+            return false;
+        }
+
+        internal void RemoveSessionVariable()
+        {
+            this.SessionState.PSVariable.Remove( "EwsSession" );
+        }
+    }
+}
